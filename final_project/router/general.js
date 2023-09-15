bf997074dc3e210db749ee813af495398fce7d84
xzheng <xiqian.zheng@tuodao.com.sg>
2023-09-15 10:28:02 +0800

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,8 +52,8 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  let filteredBooks = books.filter(items=> {
-    return items.author.toLowerCase() === req.params.author.toLowerCase();
+  let filteredBooks = Object.values(books).filter(function(items) {
+    return items.author.toLowerCase() === req.params.author.toLowerCase;
   });
   if(filteredBooks > 0){
         return res.status(200).send(JSON.stringify(filteredBooks, null, 4));
@@ -64,7 +64,7 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  let filteredBooks = books.filter(function(items) {
+  let filteredBooks = Object.values(books).filter(function(items) {
     return items.title === req.params.title;
   });
   if (filteredBooks > 0){
